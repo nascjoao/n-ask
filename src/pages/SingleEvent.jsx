f315e9c64f527b2bfc0ebc05aaac912b26ffdfeb
nascjoao <oi@joaonasc.dev>
2022-02-25 12:38:29 -0300

    feat: users can send questions

--- src/pages/SingleEvent.jsx
+++ src/pages/SingleEvent.jsx
@@ -7,23 +7,45 @@ export default function SingleEvent() {
   const [username, setUsername] = useState();
   const [askForUsername, setAskForUsername] = useState(false);
   const [getUserFromLocalStorage, setGetUserFromLocalStorage] = useState(true);
+  const [questions, setQuestions] = useState([]);
   const usernameInputRef = useRef();
+  const questionTextAreaRef = useRef();
 
   useEffect(() => {
     if (!username && !getUserFromLocalStorage) {
       setAskForUsername(true);
     }
-  }, [username]);
+  }, [username, getUserFromLocalStorage]);
 
   useEffect(() => {
     (async () => {
       const userId = localStorage.getItem('user');
-      const { data } = await supabase.from('users').select().eq('id', userId).eq('roomId', id);
-      if (data) {
-        setUsername(data[0].name);
+      if (userId) {
+        const { data: dataUser } = await supabase.from('users').select().eq('id', userId).eq('roomId', id);
+        const { data: dataQuestions } = await supabase.from('questions').select();
+        setQuestions(dataQuestions);
+        if (dataUser.length > 0) {
+          setUsername(dataUser[0].name);
+        }
       }
       setGetUserFromLocalStorage(false);
     })();
+
+    supabase
+      .from('questions')
+      .on('*', (payload) => {
+        if (payload.eventType === 'INSERT') {
+          setQuestions((currentQuestions) => [...currentQuestions, payload.new]);
+        } else {
+          setQuestions((currentQuestions) => {
+            const remainingQuestions = currentQuestions.filter(
+              (question) => question.id !== payload.old.id,
+            );
+            return remainingQuestions;
+          });
+        }
+      })
+      .subscribe();
   }, []);
 
   async function joinEvent(event) {
@@ -37,6 +59,15 @@ export default function SingleEvent() {
     setAskForUsername(false);
   }
 
+  async function sendQuestion(event) {
+    event.preventDefault();
+    await supabase.from('questions').insert({
+      userId: localStorage.getItem('user'),
+      roomId: id,
+      content: questionTextAreaRef.current.value,
+    });
+  }
+
   return (
     <>
       <div>{id}</div>
@@ -47,6 +78,17 @@ export default function SingleEvent() {
           <input type="text" ref={usernameInputRef} />
         </form>
       ) }
+      <form onSubmit={sendQuestion}>
+        <h2>Faça sua pergunta</h2>
+        { username && <strong>{username}</strong> }
+        <textarea placeholder="Poderia me dizer..." ref={questionTextAreaRef} />
+        <button type="submit">Enviar</button>
+      </form>
+      { questions.map((question) => (
+        <div key={question.id}>
+          {question.content}
+        </div>
+      )) }
     </>
   );
 }
