302151aacc8c93e97bc39ad4d7430b73db990e3e
nascjoao <oi@joaonasc.dev>
2022-03-04 14:28:37 -0300

    feat: create base html

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import imgNewEvent from '../assets/images/new-event.png';
+import imgJoinEvent from '../assets/images/join-event.png';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Link to="/room/new">
+        <img src={imgNewEvent} alt="Pessoas criando algo" />
+        <span>Criar evento</span>
+      </Link>
+      <section>
+        <img src={imgJoinEvent} alt="Pessoas criando algo" />
+        <button type="button">Entrar em evento</button>
+      </section>
+    </div>
   );
 }
