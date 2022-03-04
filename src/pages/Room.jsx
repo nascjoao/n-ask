077b0cfba88eee9c56ec66de47ca8d4e81bfc893
nascjoao <oi@joaonasc.dev>
2022-03-04 17:19:01 -0300

    feat: only show form to question if user is not the owner of the room

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useValidateRoom from '../hooks/useValidateRoom';
 import useJoinRoom from '../hooks/useJoinRoom';
 import useQuestions from '../hooks/useQuestions';
 import Loading from '../components/Loading';
 import styles from '../styles/pages/Room.module.scss';
+import supabase from '../supabaseClient';
 
 export default function Room() {
   const { id } = useParams();
   const roomIsValid = useValidateRoom(id);
   const { user, userIsRequired, FormToJoin } = useJoinRoom(id);
   const { questions, sendQuestion, questionInputRef } = useQuestions(id);
+  const [userIsOwner, setUserIsOwner] = useState();
 
-  if (!roomIsValid) return <Loading />;
+  useEffect(() => {
+    if (user && userIsOwner === undefined) {
+      (async () => {
+        const { data: [{ owner }] } = await supabase.from('rooms').select().eq('id', id);
+        setUserIsOwner(owner === user.id);
+      })();
+    }
+  }, [user]);
+
+  if (!roomIsValid || userIsOwner === undefined) return <Loading />;
 
   return (
     <div className={styles.container}>
@@ -23,12 +34,14 @@ export default function Room() {
       { userIsRequired && (
         <FormToJoin />
       ) }
-      <form onSubmit={sendQuestion} className={styles.formQuestion}>
-        <h2>Faça sua pergunta</h2>
-        { user && <strong>{user.name}</strong> }
-        <textarea placeholder="Poderia me dizer..." ref={questionInputRef} />
-        <button type="submit">Enviar</button>
-      </form>
+      { !userIsOwner && (
+        <form onSubmit={sendQuestion} className={styles.formQuestion}>
+          <h2>Faça sua pergunta</h2>
+          { user && <strong>{user.name}</strong> }
+          <textarea placeholder="Poderia me dizer..." ref={questionInputRef} />
+          <button type="submit">Enviar</button>
+        </form>
+      ) }
       { questions.map((question) => (
         <div key={question.id}>
           {question.content}
