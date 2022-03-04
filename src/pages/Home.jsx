d899a133cdc423ae69372d3bdf61a07b7b9c6c55
nascjoao <oi@joaonasc.dev>
2022-03-04 16:43:18 -0300

    feat: add modal to create room

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,38 @@
 import React, { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { v4 as generateUUID } from 'uuid';
 import imgNewEvent from '../assets/images/new-event.png';
 import imgJoinEvent from '../assets/images/join-event.png';
 import useModal from '../hooks/useModal';
+import supabase from '../supabaseClient';
 
 import styles from '../styles/pages/Home.module.scss';
 
 export default function Home() {
-  const { Modal, isOpen, onOpen } = useModal();
-  const inputRef = useRef();
+  const { Modal: JoinModal, isOpen: joinIsOpen, onOpen: joinOnOpen } = useModal();
+  const { Modal: CreateModal, isOpen: createIsOpen, onOpen: createOnOpen } = useModal();
+  const roomIdInputRef = useRef();
+  const nameInputRef = useRef();
   const navigate = useNavigate();
+
+  async function createRoom(event) {
+    event.preventDefault();
+    const userId = generateUUID();
+    const { data: [{ id: roomId }] } = await supabase.from('rooms').insert({
+      owner: userId,
+    });
+    await supabase.from('users').insert({
+      id: userId,
+      name: nameInputRef.current.value,
+      roomId,
+    });
+    navigate(`/room/${roomId}`);
+    localStorage.setItem('user', userId);
+  }
 
   function joinRoom(event) {
     event.preventDefault();
-    navigate(`/room/${inputRef.current.value}`);
+    navigate(`/room/${roomIdInputRef.current.value}`);
   }
 
   return (
@@ -21,24 +40,31 @@ export default function Home() {
       <div className={styles.home}>
         <section className={styles.cardSection}>
           <img src={imgNewEvent} alt="Pessoas criando algo" />
-          <button type="button">Criar sala</button>
+          <button type="button" onClick={createOnOpen}>Criar sala</button>
         </section>
         <section className={styles.cardSection}>
           <img src={imgJoinEvent} alt="Pessoas apresentando algo" />
-          <button type="button" onClick={onOpen}>Entrar em sala</button>
+          <button type="button" onClick={joinOnOpen}>Entrar em sala</button>
         </section>
       </div>
-      { isOpen && (
-        <Modal heading="Entrar" subheading="Insira o ID da sala">
+      { joinIsOpen && (
+        <JoinModal heading="Entrar" subheading="Insira o ID da sala">
           <form onSubmit={joinRoom} className={styles.formToJoinRoom}>
             <input
               type="text"
               placeholder="Ex.: bc908c08-dea8-485b-aa0c-07a784d3dcb6"
-              ref={inputRef}
+              ref={roomIdInputRef}
             />
             <button type="submit">Entrar</button>
           </form>
-        </Modal>
+        </JoinModal>
+      ) }
+      { createIsOpen && (
+        <CreateModal heading="Criar sala" subheading="Como gostaria de se identificar?">
+          <form onSubmit={createRoom}>
+            <input type="text" ref={nameInputRef} placeholder="Digite seu nome" />
+          </form>
+        </CreateModal>
       ) }
     </>
   );
