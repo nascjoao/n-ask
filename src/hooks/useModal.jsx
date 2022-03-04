869687d783b41790fbe3dc947458d0399c33bd69
nascjoao <oi@joaonasc.dev>
2022-03-04 16:14:19 -0300

    feat: make modal dismissable by clicking outside

--- src/hooks/useModal.jsx
+++ src/hooks/useModal.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styles from '../styles/hooks/useModal.module.scss';
 
 export default function useModal() {
   const [isOpen, setIsOpen] = useState(false);
 
+  useEffect(() => {
+    window.onclick = ({ target }) => {
+      if (target.classList.contains('overlay')) setIsOpen(false);
+    };
+  }, []);
+
   function Modal({ heading, subheading, children }) {
     return (
-      <div className={styles.overlay}>
+      <div className={`${styles.overlay} overlay`}>
         <div className={styles.modal}>
           { heading && <h1>{heading}</h1> }
           { subheading && <p>{subheading}</p> }
