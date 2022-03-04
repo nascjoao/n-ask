d1ef8fec4b2f960816d4e9031adeb4e0815bede2
nascjoao <oi@joaonasc.dev>
2022-03-04 17:02:31 -0300

    feat: create user context

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { UserProvider } from './contexts/User';
 import './styles/global.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <UserProvider>
     <App />
-  </React.StrictMode>,
+  </UserProvider>,
   document.getElementById('root'),
 );
