37b19b0a6ee85769cda70bf4f563873fade96552
nascjoao <oi@joaonasc.dev>
2022-03-04 14:50:18 -0300

    feat: create styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import imgNewEvent from '../assets/images/new-event.png';
 import imgJoinEvent from '../assets/images/join-event.png';
 
+import styles from '../styles/pages/Home.module.scss';
+
 export default function Home() {
   return (
-    <div>
-      <Link to="/room/new">
+    <div className={styles.home}>
+      <section className={styles.cardSection}>
         <img src={imgNewEvent} alt="Pessoas criando algo" />
-        <span>Criar evento</span>
-      </Link>
-      <section>
-        <img src={imgJoinEvent} alt="Pessoas criando algo" />
+        <button type="button">Criar evento</button>
+      </section>
+      <section className={styles.cardSection}>
+        <img src={imgJoinEvent} alt="Pessoas apresentando algo" />
         <button type="button">Entrar em evento</button>
       </section>
     </div>
