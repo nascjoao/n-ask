be55e11d99643c5ca18093262e0f5a9ce604abee
nascjoao <oi@joaonasc.dev>
2022-03-07 17:28:35 -0300

    refactor: question form to use chakra components

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -12,13 +12,19 @@ import {
   Flex,
   Text,
   useDisclosure,
+  Textarea,
+  Badge,
 } from '@chakra-ui/react';
+import { AnimatePresence, motion } from 'framer-motion';
 import { CloseIcon } from '@chakra-ui/icons';
 import useValidateRoom from '../hooks/useValidateRoom';
 import useJoinRoom from '../hooks/useJoinRoom';
 import useQuestions from '../hooks/useQuestions';
 import Loading from '../components/Loading';
 import supabase from '../supabaseClient';
+import Question from '../components/Question';
+
+const MotionButton = motion(Button);
 
 export default function Room() {
   const { id } = useParams();
@@ -28,6 +34,8 @@ export default function Room() {
   const [userIsOwner, setUserIsOwner] = useState();
   const cancelTerminationRef = useRef();
   const navigate = useNavigate();
+  const [remainingCharacters, setRemainingCharacters] = useState(508);
+  const [invalidQuestion, setInvalidQuestion] = useState(false);
   const {
     isOpen: terminateIsOpen,
     onOpen: terminateOnOpen,
@@ -57,6 +65,23 @@ export default function Room() {
     await supabase.from('questions').delete().match({ roomId: id });
     await supabase.from('users').delete().match({ roomId: id });
     await supabase.from('rooms').delete().match({ id });
+  }
+
+  function adjustTextareaHeight({ target }) {
+    target.setAttribute('style', 'height: auto');
+    target.setAttribute('style', `height: ${target.scrollHeight}px`);
+    setRemainingCharacters(508 - target.value.length);
+    setInvalidQuestion(false);
+  }
+
+  function submitQuestion(event) {
+    event.preventDefault();
+    if (remainingCharacters >= 0) {
+      sendQuestion(event);
+      questionInputRef.current.value = '';
+      questionInputRef.current.setAttribute('style', 'height: auto');
+      setRemainingCharacters(508);
+    } else setInvalidQuestion(true);
   }
 
   if (!roomIsValid) return <Loading />;
@@ -100,19 +125,48 @@ export default function Room() {
         </AlertDialog>
       </Flex>
       { !userIsOwner && (
-        <form onSubmit={sendQuestion}>
-          <h2>Faça sua pergunta</h2>
-          { user && <strong>{user.name}</strong> }
-          <textarea placeholder="Poderia me dizer..." ref={questionInputRef} />
-          <button type="submit">Enviar</button>
+        <form onSubmit={submitQuestion}>
+          <Flex justifyContent="space-between">
+            <Text fontWeight="bold">{`Faça uma pergunta${`, ${user && user.name}`}`}</Text>
+            <Badge
+              margin={2}
+              marginLeft={0}
+              marginRight={0}
+              colorScheme={remainingCharacters < 0 ? 'red' : 'gray'}
+            >
+              {`Caracteres restantes: ${remainingCharacters}`}
+            </Badge>
+          </Flex>
+          <Textarea
+            placeholder="Poderia me dizer..."
+            ref={questionInputRef}
+            onChange={adjustTextareaHeight}
+            overflowY="hidden"
+            isInvalid={invalidQuestion}
+            marginBottom={4}
+          />
+          <AnimatePresence>
+            { remainingCharacters !== 508 && (
+              <MotionButton
+                type="submit"
+                marginBottom={4}
+                marginLeft="auto"
+                display="block"
+                colorScheme="blue"
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -10, opacity: 0 }}
+              >
+                Enviar
+              </MotionButton>
+            )}
+          </AnimatePresence>
         </form>
       ) }
       { !questions.length ? (
         <h1>Nenhuma questão enviada</h1>
       ) : questions.map((question) => (
-        <div key={question.id}>
-          {question.content}
-        </div>
+        <Question layout key={question.id} data={question} />
       )) }
     </Container>
   );
