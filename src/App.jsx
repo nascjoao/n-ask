d3db5c49ba316871bd1f6f21d3c0d3c8b991f78a
nascjoao <oi@joaonasc.dev>
2022-03-09 08:33:45 -0300

    feat: add no match route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { Flex } from '@chakra-ui/react';
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import Home from './pages/Home';
 import Identification from './pages/Identification';
+import NotFound from './pages/NotFound';
 import Room from './pages/Room';
 import RoomEnded from './pages/RoomEnded';
 
@@ -23,7 +29,9 @@ function App() {
           <Route path="identification" element={<Identification />} />
           <Route path="not-found" element={<h1>Não existe nenhuma sala com esse ID</h1>} />
           <Route path=":id" element={<Room />} />
+          <Route path="*" index element={<Navigate to="/" />} />
         </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
